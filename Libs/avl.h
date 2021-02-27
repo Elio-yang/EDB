9be08eb2c454493cb2c3de84195c7b273c232103
Elio-yang <2925539854@qq.com>
2021-02-27 23:39:36 +0800

    stole avl & rbt part from ...

--- Libs/avl.h
+++ Libs/avl.h
@@ -15,7 +15,9 @@
 #endif
 
 #ifndef HAVE_NOT_STDDEF_H
+
 #include <stddef.h>
+
 #endif
 
 
@@ -51,17 +53,15 @@
 /*====================================================================*/
 /* avl_node - avl binary search tree                                  */
 /*====================================================================*/
-struct avl_node
-{
-	struct avl_node *left;
-	struct avl_node *right;
-	struct avl_node *parent;    /* pointing to node itself for empty node */
-	int height;                 /* equals to 1 + max height in childs */
+struct avl_node {
+        struct avl_node *left;
+        struct avl_node *right;
+        struct avl_node *parent;    /* pointing to node itself for empty node */
+        int height;                 /* equals to 1 + max height in childs */
 };
 
-struct avl_root
-{
-	struct avl_node *node;		/* root node */
+struct avl_root {
+        struct avl_node *node;                /* root node */
 };
 
 
@@ -77,7 +77,7 @@ struct avl_root
 #define AVL_DATA2NODE(d, o)    ((struct avl_node*)((size_t)(d) + (o)))
 
 #define AVL_ENTRY(ptr, type, member) \
-	((type*)AVL_NODE2DATA(ptr, AVL_OFFSET(type, member)))
+        ((type*)AVL_NODE2DATA(ptr, AVL_OFFSET(type, member)))
 
 #define avl_node_init(node) do { ((node)->parent) = (node); } while (0)
 #define avl_node_empty(node) ((node)->parent == (node))
@@ -95,27 +95,32 @@ extern "C" {
 /*--------------------------------------------------------------------*/
 
 struct avl_node *avl_node_first(struct avl_root *root);
+
 struct avl_node *avl_node_last(struct avl_root *root);
+
 struct avl_node *avl_node_next(struct avl_node *node);
+
 struct avl_node *avl_node_prev(struct avl_node *node);
 
 void avl_node_replace(struct avl_node *victim, struct avl_node *newnode,
-		struct avl_root *root);
+                      struct avl_root *root);
 
 static inline void avl_node_link(struct avl_node *node, struct avl_node *parent,
-		struct avl_node **avl_link) {
-	node->parent = parent;
-	node->height = 0;
-	node->left = node->right = NULL;
-	avl_link[0] = node;
+                                 struct avl_node **avl_link)
+{
+        node->parent = parent;
+        node->height = 0;
+        node->left = node->right = NULL;
+        avl_link[0] = node;
 }
 
 /* avl insert rebalance and erase */
 void avl_node_post_insert(struct avl_node *node, struct avl_root *root);
+
 void avl_node_erase(struct avl_node *node, struct avl_root *root);
 
 /* tear down the whole tree */
-struct avl_node* avl_node_tear(struct avl_root *root, struct avl_node **next);
+struct avl_node *avl_node_tear(struct avl_root *root, struct avl_node **next);
 
 
 /*--------------------------------------------------------------------*/
@@ -123,49 +128,48 @@ struct avl_node* avl_node_tear(struct avl_root *root, struct avl_node **next);
 /*--------------------------------------------------------------------*/
 
 #define avl_node_find(root, what, compare_fn, res_node) do {\
-		struct avl_node *__n = (root)->node; \
-		(res_node) = NULL; \
-		while (__n) { \
-			int __hr = (compare_fn)(what, __n); \
-			if (__hr == 0) { (res_node) = __n; break; } \
-			else if (__hr < 0) { __n = __n->left; } \
-			else { __n = __n->right; } \
-		} \
-	}   while (0)
+                struct avl_node *__n = (root)->node; \
+                (res_node) = NULL; \
+                while (__n) { \
+                        int __hr = (compare_fn)(what, __n); \
+                        if (__hr == 0) { (res_node) = __n; break; } \
+                        else if (__hr < 0) { __n = __n->left; } \
+                        else { __n = __n->right; } \
+                } \
+        }   while (0)
 
 
 #define avl_node_add(root, newnode, compare_fn, duplicate_node) do { \
-		struct avl_node **__link = &((root)->node); \
-		struct avl_node *__parent = NULL; \
-		struct avl_node *__duplicate = NULL; \
-		int __hr = 1; \
-		while (__link[0]) { \
-			__parent = __link[0]; \
-			__hr = (compare_fn)(newnode, __parent); \
-			if (__hr == 0) { __duplicate = __parent; break; } \
-			else if (__hr < 0) { __link = &(__parent->left); } \
-			else { __link = &(__parent->right); } \
-		} \
-		(duplicate_node) = __duplicate; \
-		if (__duplicate == NULL) { \
-			avl_node_link(newnode, __parent, __link); \
-			avl_node_post_insert(newnode, root); \
-		} \
-	}   while (0)
+                struct avl_node **__link = &((root)->node); \
+                struct avl_node *__parent = NULL; \
+                struct avl_node *__duplicate = NULL; \
+                int __hr = 1; \
+                while (__link[0]) { \
+                        __parent = __link[0]; \
+                        __hr = (compare_fn)(newnode, __parent); \
+                        if (__hr == 0) { __duplicate = __parent; break; } \
+                        else if (__hr < 0) { __link = &(__parent->left); } \
+                        else { __link = &(__parent->right); } \
+                } \
+                (duplicate_node) = __duplicate; \
+                if (__duplicate == NULL) { \
+                        avl_node_link(newnode, __parent, __link); \
+                        avl_node_post_insert(newnode, root); \
+                } \
+        }   while (0)
 
 
 /*====================================================================*/
 /* avl_tree - easy interface                                          */
 /*====================================================================*/
 
-struct avl_tree
-{
-	struct avl_root root;		/* avl root */
-	size_t offset;				/* node offset in user data structure */
-	size_t size;                /* size of user data structure */
-	size_t count;				/* node count */
-	/* returns 0 for equal, -1 for n1 < n2, 1 for n1 > n2 */
-	int (*compare)(const void *n1, const void *n2);
+struct avl_tree {
+        struct avl_root root;                /* avl root */
+        size_t offset;                                /* node offset in user data structure */
+        size_t size;                /* size of user data structure */
+        size_t count;                                /* node count */
+        /* returns 0 for equal, -1 for n1 < n2, 1 for n1 > n2 */
+        int (*compare)(const void *n1, const void *n2);
 };
 
 
@@ -176,26 +180,29 @@ struct avl_tree
  *          AVL_OFFSET(struct mystruct_t, node));
  */
 void avl_tree_init(struct avl_tree *tree,
-		int (*compare)(const void*, const void*), size_t size, size_t offset);
+                   int (*compare)(const void *, const void *), size_t size, size_t offset);
 
 void *avl_tree_first(struct avl_tree *tree);
+
 void *avl_tree_last(struct avl_tree *tree);
+
 void *avl_tree_next(struct avl_tree *tree, void *data);
+
 void *avl_tree_prev(struct avl_tree *tree, void *data);
 
 /* require a temporary user structure (data) which contains the key */
 void *avl_tree_find(struct avl_tree *tree, const void *data);
+
 void *avl_tree_nearest(struct avl_tree *tree, const void *data);
 
 /* returns NULL for success, otherwise returns conflict node with same key */
 void *avl_tree_add(struct avl_tree *tree, void *data);
 
 void avl_tree_remove(struct avl_tree *tree, void *data);
+
 void avl_tree_replace(struct avl_tree *tree, void *victim, void *newdata);
 
 void avl_tree_clear(struct avl_tree *tree, void (*destroy)(void *data));
-
-
 
 
 #ifdef __cplusplus
--- Libs/linux_rbtree.h
+++ Libs/linux_rbtree.h
@@ -96,8 +96,8 @@ static inline struct page * rb_insert_page_cache(struct inode * inode,
 -----------------------------------------------------------------------
 */
 
-#ifndef	_LINUX_RBTREE_H
-#define	_LINUX_RBTREE_H
+#ifndef        _LINUX_RBTREE_H
+#define        _LINUX_RBTREE_H
 
 #include <stddef.h>
 
@@ -122,51 +122,50 @@ static inline struct page * rb_insert_page_cache(struct inode * inode,
 #endif
 
 
-
-struct rb_node
-{
-	struct rb_node *rb_parent;
-	unsigned int rb_color;
-#define	RB_RED		0
-#define	RB_BLACK	1
-	struct rb_node *rb_right;
-	struct rb_node *rb_left;
+struct rb_node {
+        struct rb_node *rb_parent;
+        unsigned int rb_color;
+#define        RB_RED                0
+#define        RB_BLACK        1
+        struct rb_node *rb_right;
+        struct rb_node *rb_left;
 };
 
-struct rb_root
-{
-	struct rb_node *rb_node;
+struct rb_root {
+        struct rb_node *rb_node;
 };
 
-#define RB_ROOT	(struct rb_root) { NULL, }
-#define	rb_entry(ptr, type, member) container_of(ptr, type, member)
+#define RB_ROOT        (struct rb_root) { NULL, }
+#define        rb_entry(ptr, type, member) container_of(ptr, type, member)
 
 extern void rb_insert_color(struct rb_node *, struct rb_root *);
+
 extern void rb_erase(struct rb_node *, struct rb_root *);
 
 /* Find logical next and previous nodes in a tree */
 extern struct rb_node *rb_next(struct rb_node *);
+
 extern struct rb_node *rb_prev(struct rb_node *);
+
 extern struct rb_node *rb_first(struct rb_root *);
+
 extern struct rb_node *rb_last(struct rb_root *);
 
 /* Fast replacement of a single node without remove/rebalance/add/rebalance */
-extern void rb_replace_node(struct rb_node *victim, struct rb_node *newnode, 
-			    struct rb_root *root);
+extern void rb_replace_node(struct rb_node *victim, struct rb_node *newnode,
+                            struct rb_root *root);
 
-static inline void rb_link_node(struct rb_node * node, struct rb_node * parent,
-				struct rb_node ** rb_link)
+static inline void rb_link_node(struct rb_node *node, struct rb_node *parent,
+                                struct rb_node **rb_link)
 {
-	node->rb_parent = parent;
-	node->rb_color = RB_RED;
-	node->rb_left = node->rb_right = NULL;
+        node->rb_parent = parent;
+        node->rb_color = RB_RED;
+        node->rb_left = node->rb_right = NULL;
 
-	*rb_link = node;
+        *rb_link = node;
 }
 
-#endif	/* _LINUX_RBTREE_H */
-
-
+#endif        /* _LINUX_RBTREE_H */
 
 
 #endif
